7fc24c5fa0121bdbeb69dc4634259fc82f4d1d59
Imranullah Afridi <44815369+Imrulafridi@users.noreply.github.com>
2020-06-22 01:33:47 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,181 @@
+// ====== Chapter From 21 To 25 ======
+
+// ====== Task # 1
+// var firstName = prompt("Enter your First Name")
+// var lastName = prompt("Enter your Last Name")
+// var fullName = firstName + lastName
+// alert("Welcome Mr. " + fullName)
+
+// ====== Task # 2
+// var phModel = prompt("What's Your Favourite Mobile Model Name")
+// var charlength = phModel.length
+// document.write("My Favourite Phone is " + phModel)
+// document.write(" <br> Length of String :  " + charlength)
+
+// ====== Task # 3
+// var country = "Pakistani"
+// document.write("String : " + country)
+// var ind = country.indexOf("n")
+// document.write(" <br> Index of 'n' : " + ind)
+
+// ====== Task # 4
+// var text = "Hello World"
+// document.write("String : " + text)
+// var ind = text.lastIndexOf("l")
+// document.write(" <br>  Last Index of 'l' : " + ind)
+
+
+// ====== Task # 5
+// var country = "Pakistani"
+// document.write("String : " + country)
+// var ind = country.charAt(3)
+// document.write(" <br> Character at Index 3 : " + ind)
+
+
+// ====== Task # 7
+// var city1 = "Hyderabad"
+// document.write(" <br> City : " + city1)
+// var repalceCity = city1.replace("Hyder" , "Islam" )
+// document.write(" <br> After Replacement  : " + repalceCity)
+
+
+// ====== Task # 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// document.write(" Message is : " + message)
+// var repalce = message.replace( /and/g , "&")
+// document.write(" <br> New Message is : " + repalce)
+
+
+// ====== Task # 9
+// var str = "472"
+// document.write("Value : " + str )
+// document.write(" <br> Type : " + typeof(str) )
+// var num = 472
+// document.write(" <br> Value : " + num )
+// document.write(" <br> Type : " + typeof(num) )
+
+
+// ====== Task # 10
+// var input = prompt("write Your Messsage")
+// document.write(" User Input : " + input)
+// var upinput = input.toUpperCase()
+// document.write(" <br> Upper Case : " + upinput)
+
+
+// ====== Task # 11
+// var input = prompt("write Your Messsage")
+// document.write(" User Input : " + input)
+// var firstchar = input.slice(0,1)
+// firstchar = firstchar.toUpperCase()
+// var otherchar = input.slice(1)
+// otherchar = otherchar.toLowerCase()
+// fullchar = firstchar + otherchar
+// document.write(" <br> Title Case : " + fullchar)
+
+
+// ====== Task # 11
+
+
+
+
+
+
+
+// ====== Chapter From 26 To 30 ======
+// ====== Task # 1
+// var num = prompt("Enter Your Number")
+// document.write("Number : " + num)
+// roundnum = Math.round(num)
+// document.write("<br> Round Off Value : " + roundnum)
+// document.write("<br> Floor Value : " + roundnum)
+// roundnum = Math.ceil(num)
+// document.write("<br> Ceil Value : " + roundnum)
+
+
+// ====== Task # 2
+// var num = prompt("Enter Your Number")
+// document.write("Number : " + num)
+// num = Math.round(num)
+// document.write("<br> Round Off Value : " + num)
+// num = Math.floor(num)
+// document.write("<br> Floor Value : " + num)
+// num = Math.ceil(num)
+// document.write("<br> Ceil Value : " + num)
+
+
+// ====== Task # 3
+// var num = prompt("Enter Your Number")
+// var absnum = Math.abs(num)
+// document.write("The absolute Value of " + num + " is " + absnum)
+
+
+// ====== Task # 4
+// var num = Math.floor((Math.random() * 6) + 1);
+// document.write("Random Dice Value is : " + num)
+
+
+// ====== Task # 5
+// var num = Math.floor((Math.random() * 2) + 1);
+// document.write("Random Coin Value is : " + num)
+// if(num === 2){
+//     document.write("<br> Heads")
+// }else(
+//     document.write("<br> Tails")
+// )
+
+
+// ====== Task # 6
+// var num = Math.floor((Math.random() * 100) + 1);
+// document.write("Random Number Between 1 and 100 is : " + num)
+
+
+// ====== Task # 7
+// var weight = prompt("Enter Your Weight in Kg")
+// weight = parseInt(weight)
+// document.write("The weight of user is : " + weight)
+
+
+// ====== Task # 8
+// var num = prompt("Enter Your Number between 1 to 10")
+// var secret = Math.floor((Math.random() * 10) + 1)
+// if( secret == num){
+//     alert("Congratulation You are right")
+// }else{
+//     alert("Try Again")
+// }
+
+
+
+
+// ====== Chapter From 31 To 34 ======
+// ====== Task # 1
+// var a = new Date()
+// document.write(a) 
+
+
+// ====== Task # 2
+var a = new Date()
+a.getMonth(5)
+alert(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====== Task # 1
+// ====== Task # 1
+// ====== Task # 1
+// ====== Task # 1
+// ====== Task # 1
+// ====== Task # 1
+
+
+
